36392b63394db724b26da60d3dc1ee70a7732fe7
Alex Bates <alexlbates@gmail.com>
2023-01-02 13:01:56 -0500

    change phrase generation to use random bag instead of pure random, to avoid frequent duplicates

--- phrases.go
+++ phrases.go
@@ -6,13 +6,24 @@ import (
 	"time"
 )
 
+// Generate a string that is [n] words long.
+// This string is generated from the static word list that is provided in this file. 
+// Words are selected by random bag. 
+// If the desired length is greater than the bag, the bag is reshuffled. 
 func GenerateTypingPhrase(length int) string {
-	r := rand.New(rand.NewSource(time.Now().UnixMicro()))
 
-	generated := make([]string, length)
-	for i := 0; i < length; i++ {
-		generated[i] = wordlist[r.Intn(length)]
-	}
+    shuffled := shuffleStringArray(wordlist)
+    generated := make([]string, length)
+
+    actualpos := 0 
+    for i:= 0; i < length; i ++ {
+        if actualpos >= len(shuffled) {
+            shuffled = shuffleStringArray(shuffled)
+            actualpos = 0 
+        }
+        generated[i] = shuffled[actualpos]
+        actualpos = actualpos + 1
+    }
 
 	genString := ""
 	for _, x := range generated {
@@ -22,9 +33,28 @@ func GenerateTypingPhrase(length int) string {
 	return strings.TrimSpace(genString)
 }
 
+// 
+func shuffleStringArray(inputArray []string) []string {
+    r := rand.New(rand.NewSource(time.Now().UnixMicro()))
+    out_arr := make([]string, len(inputArray))
+
+    for i, val := range (inputArray) {
+        out_arr[i] = val 
+    }
+    
+    for i := 0;  i < len(out_arr); i++ {
+        randInt := r.Intn(len(out_arr))
+        swap := out_arr[i]
+        out_arr[i] = out_arr[randInt] 
+        out_arr[randInt] = swap 
+    }
+
+    return out_arr 
+}
+
 //200 most common english words.
 //TODO replace this with a text file that is loaded into memory
-var wordlist = [...]string{
+var wordlist = []string{
 	"the", "be", "of", "and", "a", "to", "in", "he", "have", "it", "that", "for", "they", "I", "with", "as",
 	"not", "on", "she", "at", "by", "this", "we", "you", "do", "but", "from", "or", "which", "one", "would", "all",
 	"will", "there", "say", "who", "make", "when", "can", "more", "if", "no", "man", "out", "other", "so", "what", "time",
