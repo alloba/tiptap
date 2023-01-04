658f7eab55f8484d7229b7fe0e47c398de639868
Alex Bates <alexlbates@gmail.com>
2023-01-04 11:22:47 -0500

    reorganize typing view into single struct, in preparation for creating additional views

--- main.go
+++ main.go
@@ -5,10 +5,19 @@ import (
 	"os"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
+type Style struct {
+	containerStyle lipgloss.Style
+	inputStyle     lipgloss.Style
+	phraseStyle    lipgloss.Style
+	errorStyle     lipgloss.Style
+	cursorStyle    lipgloss.Style
+}
+
 func main() {
-	p := tea.NewProgram(initialModel())
+	p := tea.NewProgram(NewTypingModel())
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("Alas, there's been an error: %v", err)
 		os.Exit(1)
--- typing.go
+++ typing.go
@@ -7,63 +7,30 @@ import (
 	"github.com/charmbracelet/lipgloss"
 )
 
-
-//TODO: I think the next step is to organize everything to exist within a logical container. 
-//      Like the phrase and userinput of the current type model would be extracted out into its own struct, 
-//      as would the styling. 
-//      then both of those would be wrapped in another struct, the "view", 
-//      which would mostly just pass render and update events to child structs.
-//
-//      This will give me a good example of what other views should look like going forward (start screen, stats screen). 
-
 type TypeModel struct {
 	phrase    string
 	userInput string
 	style     Style
 }
 
-type Style struct {
-	containerStyle lipgloss.Style
-	inputStyle     lipgloss.Style
-	phraseStyle    lipgloss.Style
-	errorStyle     lipgloss.Style
-	cursorStyle    lipgloss.Style
+func NewTypingModel() TypeModel {
+	style := Style{
+		containerStyle: lipgloss.NewStyle().Bold(true).PaddingTop(1).PaddingLeft(2),
+		inputStyle:     lipgloss.NewStyle().Background(lipgloss.Color("#16001E")).Foreground(lipgloss.Color("#DE639A")),
+		phraseStyle:    lipgloss.NewStyle().Background(lipgloss.Color("#16001E")).Foreground(lipgloss.Color("#F7B2B7")),
+		errorStyle:     lipgloss.NewStyle().Background(lipgloss.Color("#FFFFFF")).Foreground(lipgloss.Color("#7F2982")),
+		cursorStyle:    lipgloss.NewStyle().Foreground(lipgloss.Color("#FAFA00")),
+	}
+
+	return TypeModel{
+		phrase:    GenerateTypingPhrase(20),
+		userInput: "",
+		style:     style,
+	}
 }
 
 func (model TypeModel) Init() tea.Cmd {
 	return nil
-}
-
-// Handle all user input events during the update loop.
-func processUserInputEvents(msg tea.KeyMsg, model TypeModel) (TypeModel, tea.Cmd) {
-	switch msg.Type {
-
-	case tea.KeyCtrlC, tea.KeyEscape:
-		return model, tea.Quit
-
-	case tea.KeyRunes, tea.KeySpace:
-		model.userInput += msg.String()
-
-	case tea.KeyBackspace:
-		if len(model.userInput) == 0 {
-			return model, nil
-		}
-		model.userInput = model.userInput[0 : len(model.userInput)-1]
-
-	case tea.KeyCtrlH: //ctrl + backspace
-		if len(model.userInput) == 0 {
-			return model, nil
-		}
-
-		var prevWordIndex = strings.LastIndex(model.userInput, " ")
-		if prevWordIndex == -1 {
-			model.userInput = "" // only one word exists, clear the entire field.
-			return model, nil
-		}
-
-		model.userInput = model.userInput[0:prevWordIndex]
-	}
-	return model, nil
 }
 
 func (model TypeModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
@@ -106,6 +73,38 @@ func (model TypeModel) View() string {
 	return model.style.containerStyle.Render(doc)
 }
 
+// Handle all user input events during the update loop.
+func processUserInputEvents(msg tea.KeyMsg, model TypeModel) (TypeModel, tea.Cmd) {
+	switch msg.Type {
+
+	case tea.KeyCtrlC, tea.KeyEscape:
+		return model, tea.Quit
+
+	case tea.KeyRunes, tea.KeySpace:
+		model.userInput += msg.String()
+
+	case tea.KeyBackspace:
+		if len(model.userInput) == 0 {
+			return model, nil
+		}
+		model.userInput = model.userInput[0 : len(model.userInput)-1]
+
+	case tea.KeyCtrlH: //ctrl + backspace
+		if len(model.userInput) == 0 {
+			return model, nil
+		}
+
+		var prevWordIndex = strings.LastIndex(model.userInput, " ")
+		if prevWordIndex == -1 {
+			model.userInput = "" // only one word exists, clear the entire field.
+			return model, nil
+		}
+
+		model.userInput = model.userInput[0:prevWordIndex]
+	}
+	return model, nil
+}
+
 // Incorrectly typed characters overwrite target characters in the view.
 // This makes it easy to see what exactly was typed, but makes it harder to recover quickly.
 func renderTechnique_errorPriority(model TypeModel, i int) string {
@@ -141,19 +140,5 @@ func renderTechnique_errorPriority(model TypeModel, i int) string {
 
 	default:
 		panic("view render unreachable statement")
-	}
-}
-
-func initialModel() TypeModel {
-	return TypeModel{
-		phrase:    GenerateTypingPhrase(300),
-		userInput: "",
-		style: Style{
-			containerStyle: lipgloss.NewStyle().Bold(true).PaddingTop(1).PaddingLeft(2),
-			inputStyle:     lipgloss.NewStyle().Background(lipgloss.Color("#16001E")).Foreground(lipgloss.Color("#DE639A")),
-			phraseStyle:    lipgloss.NewStyle().Background(lipgloss.Color("#16001E")).Foreground(lipgloss.Color("#F7B2B7")),
-			errorStyle:     lipgloss.NewStyle().Background(lipgloss.Color("#FFFFFF")).Foreground(lipgloss.Color("#7F2982")),
-			cursorStyle:    lipgloss.NewStyle().Foreground(lipgloss.Color("#FAFA00")),
-		},
 	}
 }
